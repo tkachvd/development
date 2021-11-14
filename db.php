854e5bc57996b8471d080ddb9f96960140013f1b
tkachvd <93051155+tkachvd@users.noreply.github.com>
2021-11-14 21:01:13 +0300

    Change password in connection string

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","vad181003","register");
+$con = mysqli_connect("localhost","root","192549","register");
 // Check connection
 if (mysqli_connect_errno())
   {
